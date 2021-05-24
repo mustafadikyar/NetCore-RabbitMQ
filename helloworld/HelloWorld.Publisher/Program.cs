344c02f0ce3612d3eaeda3a377358053a05f3878
Mustafa Dikyar <mustafadikyar1@gmail.com>
2021-05-24 15:49:36 +0300

    work-queues

--- helloworld/HelloWorld.Publisher/Program.cs
+++ helloworld/HelloWorld.Publisher/Program.cs
@@ -1,6 +1,8 @@
 ﻿using RabbitMQ.Client;
 using System;
+using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace HelloWorld.Publisher
 {
@@ -23,16 +25,20 @@ namespace HelloWorld.Publisher
             //autodelete : en az bir tüketiciye sahip olan kuyruk, son tüketici abonelikten çıktığında silinir.
             //arguments : (isteğe bağlı) eklentiler ve mesaj TTL'si, kuyruk uzunluğu sınırı vb. Gibi aracıya özgü özellikler tarafından kullanılır
             channel.QueueDeclare(queue: "hello-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            //Kuyruk var ise kullanır yok ise sıfırdan oluşturur.
 
-            string message = "Hello World!"; //gönderilecek mesaj
-            byte[] body = Encoding.UTF8.GetBytes(message); //mesajlar byte array tipinde taşınır.
+            Enumerable.Range(1, 50).ToList().ForEach(item => //kuyruğa toplu mesaj gönderdik.
+            {
+                string message = $"Hello World! Step {item}"; //gönderilecek mesaj
+                byte[] body = Encoding.UTF8.GetBytes(message); //mesajlar byte array tipinde taşınır.
 
-            //exchange : belirtmezsek doğrudan kuyruğa gönderilir.
-            //routingkey : yönlendirilecek kuyruk adı
-            //basicproperties : 
-            //body : veri
-            channel.BasicPublish(exchange: string.Empty, routingKey: "hello-queue", basicProperties: null, body: body);
-            Console.WriteLine("Mesaj gönderildi.");
+                //exchange : belirtmezsek doğrudan kuyruğa gönderilir.
+                //routingkey : yönlendirilecek kuyruk adı
+                //basicproperties : 
+                //body : veri
+                channel.BasicPublish(exchange: string.Empty, routingKey: "hello-queue", basicProperties: null, body: body);
+                Console.WriteLine($"Gönderilen mesaj : {item}");
+            });
             Console.ReadLine();
 
             //bir bağlantı üzerinden birden fazla kanal oluşturabiliriz.
--- helloworld/HelloWorld.Subscriber/Program.cs
+++ helloworld/HelloWorld.Subscriber/Program.cs
@@ -26,19 +26,32 @@ namespace HelloWorld.Subscriber
             //parametrelerin tutarlılığı önemli.
             //channel.QueueDeclare(queue: "hello-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
+
+            //mesajlara erişim kıstasları
+            //prefetchSize : mesaj boyutu (0 : herhangi bir boyuttaki mesaj)
+            //prefetchCount : mesajlar tüketicilere kaçar kaçar gönderilecek.
+            //global :  toplamda tüm kullanıcılara kaçar kaçar gönderilecek. 3 consumer ve 6 mesaj için her consumer'a ikişer tane mesaj gönderir.
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+
             //tüketici örneği oluşturuyoruz. 
             EventingBasicConsumer consumer = new(channel);
 
             //queue : izlenecek kuyruk
             //autoAck : (true : mesaj işlendiği zaman (başarılı başarısız farketmez) kuyruktan silinir. | false : işlem başarılıysa kuyruktan silmesini biz bildiriyoruz.)
             //consumer : tüketici
-            channel.BasicConsume(queue: "hello-queue", autoAck: true, consumer: consumer);
+            channel.BasicConsume(queue: "hello-queue", autoAck: false, consumer: consumer);
 
             //mesajı dinleyen event.
             consumer.Received += (object sender ,BasicDeliverEventArgs e) =>
             {
                 var message = Encoding.UTF8.GetString(e.Body.ToArray()); //yakalanan mesaj
                 Console.WriteLine($"Gelen mesaj : {message}");
+
+                //işlemin kuyruktan silinmesi için haber yolluyoruz.
+                //deliveryTag : gelen mesajın adresi
+                //multiple : eğer kuyrukta işlem tamamlanmış fakat silinmemiş işlemler var ise onları siler.
+                channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false); 
             };
             Console.ReadLine();
         }
