f7190c99ba8efea01c1f71b4a72fe89b5d8cd4c7
Mustafa Dikyar <mustafadikyar1@gmail.com>
2021-05-24 17:08:17 +0300

    Fanout-Exchange

---
+++ exchanges/fanoutexchange/FanoutExchange.Publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace FanoutExchange.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new(); //rabbitmq için bir bağlantı örneği oluşturduk.
+
+            //bağlantı adresi için rabbitmq-cloud kullanıyoruz.
+            factory.Uri = new Uri("amqps://gpgmtthl:dtDiiqd4qyog_b3f7WjHYjVWlYvai-ff@baboon.rmq.cloudamqp.com/gpgmtthl"); //bağlantı adresi
+
+            using IConnection connection = factory.CreateConnection(); //bağlantı açıyoruz.
+            using IModel channel = connection.CreateModel(); //kanal oluşturuyoruz.           
+
+            //exchange : adı
+            //type : tipi
+            //durable : kuyruğun nerede tutulacakğı (false : memory | true : disk)
+            //autodelete : en az bir tüketiciye sahip olan kuyruk, son tüketici abonelikten çıktığında silinir.
+            //arguments : (isteğe bağlı) eklentiler ve mesaj TTL'si, kuyruk uzunluğu sınırı vb. Gibi aracıya özgü özellikler tarafından kullanılır.
+            channel.ExchangeDeclare(exchange: "logs-fanout", type: ExchangeType.Fanout, durable: false, autoDelete: false, arguments: null);
+
+
+            Enumerable.Range(1, 50).ToList().ForEach(item => //kuyruğa toplu mesaj gönderdik.
+            {
+                string message = $"Log, Step {item}"; //gönderilecek mesaj
+                byte[] body = Encoding.UTF8.GetBytes(message); //mesajlar byte array tipinde taşınır.
+
+                //exchange : belirtmezsek doğrudan kuyruğa gönderilir.
+                //routingkey : yönlendirilecek kuyruk adı
+                //basicproperties : 
+                //body : veri
+                channel.BasicPublish(exchange: "logs-fanout", routingKey: string.Empty, basicProperties: null, body: body);
+                Console.WriteLine($"Gönderilen mesaj : {item}");
+            });
+            Console.ReadLine();
+
+            //bir bağlantı üzerinden birden fazla kanal oluşturabiliriz.
+        }
+    }
+}
---
+++ exchanges/fanoutexchange/FanoutExchange.Subscriber/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace FanoutExchange.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new(); //rabbitmq için bir bağlantı örneği oluşturduk.
+            factory.Uri = new Uri("amqps://gpgmtthl:dtDiiqd4qyog_b3f7WjHYjVWlYvai-ff@baboon.rmq.cloudamqp.com/gpgmtthl"); //bağlantı adresi
+
+            using IConnection connection = factory.CreateConnection(); //bağlantı açıyoruz.
+            using IModel channel = connection.CreateModel(); //kanal oluşturuyoruz.
+
+            //kuyruğu kalıcı hale getirmek için
+            //var queueName = "save-queue";
+            //channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false);
+
+
+            //rasgele kuyruk ismi oluşturur. isim çakışmalarını önlemek için.
+            var randomQueueName = channel.QueueDeclare().QueueName;
+
+            //uygulama her ayağa kalktığında bir kuyruk oluşacak.
+            channel.QueueBind(queue: randomQueueName, exchange: "logs-fanout", routingKey: string.Empty, arguments: null);
+
+            //mesajlara erişim kıstasları
+            //prefetchSize : mesaj boyutu (0 : herhangi bir boyuttaki mesaj)
+            //prefetchCount : mesajlar tüketicilere kaçar kaçar gönderilecek.
+            //global :  toplamda tüm kullanıcılara kaçar kaçar gönderilecek. 3 consumer ve 6 mesaj için her consumer'a ikişer tane mesaj gönderir.
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+
+            //tüketici örneği oluşturuyoruz. 
+            EventingBasicConsumer consumer = new(channel);
+
+            //queue : izlenecek kuyruk
+            //autoAck : (true : mesaj işlendiği zaman (başarılı başarısız farketmez) kuyruktan silinir. | false : işlem başarılıysa kuyruktan silmesini biz bildiriyoruz.)
+            //consumer : tüketici
+            channel.BasicConsume(queue: randomQueueName, autoAck: false, consumer: consumer);
+            Console.WriteLine("Mesaj bekleniyor...");
+
+            //mesajı dinleyen event.
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray()); //yakalanan mesaj
+                Console.WriteLine($"Gelen mesaj : {message}");
+
+                //işlemin kuyruktan silinmesi için haber yolluyoruz.
+                //deliveryTag : gelen mesajın adresi
+                //multiple : eğer kuyrukta işlem tamamlanmış fakat silinmemiş işlemler var ise onları siler.
+                channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+            };
+            Console.ReadLine();
+        }
+    }
+}
