829c007c5faad9e327919f5e86676c8d541f4c60
Mustafa Dikyar <mustafadikyar1@gmail.com>
2021-05-26 11:46:42 +0300

    Topic-Exchange

---
+++ exchanges/topicexchange/TopicExchange.Publisher/Program.cs
@@ -0,0 +1,63 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace TopicExchange.Publisher
+{
+    public enum LogType
+    {
+        Critical = 1,
+        Error,
+        Warning,
+        Info
+    }
+
+    /// <summary>
+    /// Detaylı routing yapısına ihtiyacımız olduğu zamanlarda kullanırız.
+    /// Direct Exchange'den farkı routing yapısıdır.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new(); //rabbitmq için bir bağlantı örneği oluşturduk.
+
+            //bağlantı adresi için rabbitmq-cloud kullanıyoruz.
+            factory.Uri = new Uri("amqps://gpgmtthl:dtDiiqd4qyog_b3f7WjHYjVWlYvai-ff@baboon.rmq.cloudamqp.com/gpgmtthl"); //bağlantı adresi
+
+            using IConnection connection = factory.CreateConnection(); //bağlantı açıyoruz.
+            using IModel channel = connection.CreateModel(); //kanal oluşturuyoruz.           
+
+            //exchange : adı
+            //type : exchange tipi
+            //durable : kuyruğun nerede tutulacakğı (false : memory | true : disk)
+            //autodelete : en az bir tüketiciye sahip olan kuyruk, son tüketici abonelikten çıktığında silinir.
+            //arguments : (isteğe bağlı) eklentiler ve mesaj TTL'si, kuyruk uzunluğu sınırı vb. Gibi aracıya özgü özellikler tarafından kullanılır.
+            channel.ExchangeDeclare(exchange: "logs-topic", type: ExchangeType.Topic, durable: false, autoDelete: false, arguments: null);
+
+            Random random = new Random();
+            Enumerable.Range(1, 50).ToList().ForEach(item => //kuyruğa toplu mesaj gönderdik.
+            {
+                LogType log1 = (LogType)random.Next(1, 5);
+                LogType log2 = (LogType)random.Next(1, 5);
+                LogType log3 = (LogType)random.Next(1, 5);
+
+                //Örnek root
+                var routeKey = $"{log1}.{log2}.{log3}";
+                
+                string message = $"Log type : {log1}-{log2}-{log3}"; //gönderilecek mesaj
+                byte[] body = Encoding.UTF8.GetBytes(message); //mesajlar byte array tipinde taşınır.
+
+                //exchange : belirtmezsek doğrudan kuyruğa gönderilir.
+                //routingkey : yönlendirilecek kuyruk adı
+                //basicproperties : 
+                //body : veri
+                channel.BasicPublish(exchange: "logs-topic", routingKey: routeKey, basicProperties: null, body: body);
+                Console.WriteLine($"{message}");
+            });
+            Console.ReadLine();
+            //bir bağlantı üzerinden birden fazla kanal oluşturabiliriz.
+        }
+    }
+}
---
+++ exchanges/topicexchange/TopicExchange.Subscriber/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.IO;
+using System.Text;
+
+namespace TopicExchange.Subscriber
+{
+    class Program
+    {
+        /// <summary>
+        /// Kuyrukları ihtiyacımıza göre consumer tarafında oluşturuyoruz.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new(); //rabbitmq için bir bağlantı örneği oluşturduk.
+            factory.Uri = new Uri("amqps://gpgmtthl:dtDiiqd4qyog_b3f7WjHYjVWlYvai-ff@baboon.rmq.cloudamqp.com/gpgmtthl"); //bağlantı adresi
+
+            using IConnection connection = factory.CreateConnection(); //bağlantı açıyoruz.
+            using IModel channel = connection.CreateModel(); //kanal oluşturuyoruz.
+
+            //mesajlara erişim kıstasları
+            //prefetchSize : mesaj boyutu (0 : herhangi bir boyuttaki mesaj)
+            //prefetchCount : mesajlar tüketicilere kaçar kaçar gönderilecek.
+            //global :  toplamda tüm kullanıcılara kaçar kaçar gönderilecek. 3 consumer ve 6 mesaj için her consumer'a ikişer tane mesaj gönderir.
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            //tüketici örneği oluşturuyoruz. 
+            EventingBasicConsumer consumer = new(channel);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            //*.*.Warning | *.Error.*
+            //Info.# | #.Warning 
+            var routeKey = "*.Error.*"; //filtre 
+            channel.QueueBind(queueName, "logs-topic", routeKey);
+
+            //queue : izlenecek kuyruk
+            //autoAck : (true : mesaj işlendiği zaman (başarılı başarısız farketmez) kuyruktan silinir. | false : işlem başarılıysa kuyruktan silmesini biz bildiriyoruz.)
+            //consumer : tüketici
+            channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+            Console.WriteLine("Mesaj bekleniyor...");
+
+            //mesajı dinleyen event.
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray()); //yakalanan mesaj
+                Console.WriteLine($"Gelen mesaj : {message}");
+                File.AppendAllText("log-critical.txt", message + "\n");
+
+                //işlemin kuyruktan silinmesi için haber yolluyoruz.
+                //deliveryTag : gelen mesajın adresi
+                //multiple : eğer kuyrukta işlem tamamlanmış fakat silinmemiş işlemler var ise onları siler.
+                channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+            };
+            Console.ReadLine();
+        }
+    }
+}
